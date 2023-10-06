262e1d9b436292359285202c36e2b6945378a646
Norbory <mandros.angelof@pucp.edu.pe>
2023-10-06 13:27:32 -0500

    Cambio

--- src/routes/Page404.routes.tsx
+++ src/routes/Page404.routes.tsx
@@ -3,7 +3,7 @@ export function Page404(){
     return(
         <div>
             <h1 className="text-4xl font-bold self-center mb-4">
-                Esta pagina no existe
+                Esta pagina no existe 404
             </h1>
         </div>
     );
