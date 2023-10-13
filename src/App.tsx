fdc26a2e03e5685cc15e34df1a6336abad2044ab
Norbory <mandros.angelof@pucp.edu.pe>
2023-10-13 05:28:55 -0500

    Cambios para style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import {Sidebar} from "./components/sidebar"
 // import Pantalla from './Pantalla';
-=======
-import { Outlet } from "react-router-dom";
-import { Sidebar } from "./components/sidebar"
-import { Dashboard } from "./routes";
->>>>>>> 94209ca181989b9e54c975c7a089d3ecdca9d589
 import './App.css'
 import Pantalla from "./Pantalla"
 
@@ -14,17 +8,7 @@ function App() {
   return (
     <div className="w-full flex-col overflow-x-hidden">
       <Sidebar/>
-<<<<<<< HEAD
-      <div className="w-1/4"><Pantalla/> </div>
-
-     
-      
-=======
-      <div>
-        <Dashboard/>
-        <Outlet/>
-      </div>
->>>>>>> 94209ca181989b9e54c975c7a089d3ecdca9d589
+      <Pantalla/>
     </div>
   )
 }
